b99a325d3bd22acb98092b9c00d8ce37bc12c94a
Reis <153527719+Reizs@users.noreply.github.com>
2025-11-29 09:35:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import jogosRoutes from "./src/routes/games.js"; // ajuste conforme seu nome real
+import jogosRoutes from "./src/routes/games.js";
 
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// public
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/api/jogos", jogosRoutes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log("Rodando na porta", port));
-
+export default app;
