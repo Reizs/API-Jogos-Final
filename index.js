33b3aa7b6f104982ff18480170188d29932e9709
Reis <153527719+Reizs@users.noreply.github.com>
2025-11-29 08:34:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Servir arquivos estáticos (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
